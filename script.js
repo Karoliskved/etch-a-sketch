3caca1c81b4b92938aa62619af4aa934d106435e
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-06 20:31:03 +0300

    add grid resizing by inputing a desired value, add grid item changing on mouseover, change style for the grid to fill the allowed space completly

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 let container=document.querySelector(".grid")
-const div=document.createElement('div')
-div.classList.add('griditem')
-for(let j=0; j<16; j++){
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+function cngColor(event){
+
+    event.target.style.backgroundColor="black"
+}
+function changeColor(){
+    let gridItems=document.querySelectorAll(".griditem");
+    gridItems.forEach(element => {
+    element.addEventListener('mouseover', cngColor);
+})
+}
+
+function generateGrid(n){
+    removeAllChildNodes(container)
+    const div=document.createElement('div')
+    div.classList.add('griditem')
+    for(let j=0; j<n; j++){
     const divrow=document.createElement('div')
     divrow.classList.add('gridRow')
     container.appendChild(divrow)
-    for(let i=0; i<16; i++){
+    for(let i=0; i<n; i++){
         const div=document.createElement('div')
         div.classList.add('griditem')
         divrow.appendChild(div)
-        
     }
 }
+    changeColor()
+}
+
+
+let button=document.querySelector(".resize")
+button.addEventListener('click', ()=>{
+    test=prompt("enter grid size");
+    generateGrid(parseInt(test));
+
+})
+
