eec40ab1f9d42f272e2afb4ae26f77c370ef8beb
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-08-28 01:27:18 +0300

    add the ability to wipe the grid

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # etch-a-sketch
 
+basic etch-a-sketch made using vanilla html, css, javascript.
+
+Click resize to set gride size or reset the grid.
+
+Left mouse button must be held-down to draw.
+
+Click on one of the bottom colors to choose draw color (default is black).
+
 Link to project https://karoliskved.github.io/etch-a-sketch/
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let color = 'black';
+let size = 0;
 const container = document.querySelector('.grid');
 const colorContainer = document.querySelector('.colorContainer');
 const colorChoices = document.querySelectorAll('.colorSelect');
+const body = document.querySelector('body');
 colorChoices.forEach((color) => {
   color.addEventListener('click', chooseColor);
 });
+
 function chooseColor(event) {
   color = event.target.id;
 }
@@ -13,9 +16,11 @@ function removeAllChildNodes(parent) {
     parent.removeChild(parent.firstChild);
   }
 }
+//set color of an individual grid element
 function cngColor(event) {
   event.target.style.backgroundColor = color;
 }
+// add event listener to all grid elements
 function changeColor() {
   let gridItems = document.querySelectorAll('.griditem');
   gridItems.forEach((element) => {
@@ -43,15 +48,16 @@ function generateGrid(n) {
       divrow.appendChild(div);
     }
   }
-  /*document.querySelector('body').addEventListener('click', () => {
-    console.log('test');
-  });*/
-  document.querySelector('body').addEventListener('mousedown', changeColor);
-  document.querySelector('body').addEventListener('mouseup', removeChangeColor);
+  body.addEventListener('mousedown', changeColor);
+  body.addEventListener('mouseup', removeChangeColor);
 }
 
-let button = document.querySelector('.resize');
-button.addEventListener('click', () => {
-  test = prompt('enter grid size');
-  generateGrid(parseInt(test));
+const resizeButton = document.querySelector('.resize');
+resizeButton.addEventListener('click', () => {
+  size = prompt('enter grid size');
+  generateGrid(parseInt(size));
+});
+const wipeButton = document.querySelector('.wipe');
+wipeButton.addEventListener('click', () => {
+  generateGrid(parseInt(size));
 });
